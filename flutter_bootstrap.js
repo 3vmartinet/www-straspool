c7eda884a168a8e9f652facc39fef0d4d122a6bc
3vmartinet <mofx71@gmail.com>
2025-01-29 18:03:15 +0100

    Fix frequentations

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3729985970"
+    serviceWorkerVersion: "3574360024"
   }
 });
