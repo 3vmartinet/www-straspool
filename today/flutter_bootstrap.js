b7184345bd51144e212d865b4a7126642f7d3e13
3vmartinet <mofx71@gmail.com>
2025-03-02 12:11:54 +0100

    Re-generate Today In History bis

--- today/flutter_bootstrap.js
+++ today/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1122500128"
+    serviceWorkerVersion: "4208468896"
   }
 });
