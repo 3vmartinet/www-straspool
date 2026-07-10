85cce771413d73d50063d4dd7db2c4fe28154f3f
3vmartinet <mofx71@gmail.com>
2026-07-10 17:35:30 +0200

    DipAlert update 1.4.2

--- dipalert/flutter_bootstrap.js
+++ dipalert/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3712671804" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "251241351" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
